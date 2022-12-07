7be479dc02aa6473f94421d074e70a8a0a1ada99
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-12-07 20:43:34 -0300

    More attemps of E1 problem of codeforces round 828 div 3

--- Codeforces Round #828 (Div. 3)/E1.cpp
+++ Codeforces Round #828 (Div. 3)/E1.cpp
@@ -1,19 +1,19 @@
 // https://codeforces.com/contest/1744/problem/E1
 
 #include <iostream>
-// attemps
+
 using namespace std;
 
 pair<int, int> solve1(int a, int b, int c, int d) {
+    int ab = a * b;
     for (int x = a + 1; x <= c; x++) {
-        for (int y = b + 1; y <= d; y++) {
-            int rem = (x * y) % (a * b);
+        for (int y = b + 1; y <= d;) {
+            int rem = (x * y) % ab;
             if (rem == 0) {
                 return {x, y};
             }
-            else if (rem / x > 0) {
-                y += rem / x - 1;
-            }
+            int y_sum = (ab - rem) / x;
+            y += y_sum > 0 ? y_sum : 1;
         }
     }
     return {-1, -1};
