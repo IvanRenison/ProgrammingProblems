a5828f5df43804053b6b590692ba6857f38386f4
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-02-16 14:57:07 -0300

    A, B and C problems of Educational Codeforces Round 135

---
+++ Templates/TemplateN.cpp
@@ -0,0 +1,30 @@
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned int uint;
+
+static inline void parse(void) {
+
+}
+
+static inline void solve(void) {
+
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+
+  cin >> t;
+
+  for (uint i = 0; i < t; i++) {
+    solve(parse()) >> cout;
+  }
+
+  return EXIT_SUCCESS;
+}
