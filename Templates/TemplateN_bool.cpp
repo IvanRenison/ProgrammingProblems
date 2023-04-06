94bac682d7f735f2d9d807ba3ad0a03dd33321d3
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-04-06 16:00:14 -0300

    Add new templates

---
+++ Templates/TemplateN_bool.cpp
@@ -0,0 +1,35 @@
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+#define fore(i, a, b) for (ull i = a; i < b; i++)
+
+void parse1(void) {
+
+}
+
+bool solve1(void) {
+
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  ull t;
+
+  cin >> t;
+
+  fore(i, 0, t) {
+    if (solve1(parse1())) {
+      cout << "Yes";
+    } else {
+      cout << "No";
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
---
+++ Templates/template_bool.py
@@ -0,0 +1,17 @@
+#
+
+def parse():
+    pass
+
+def solve():
+    pass
+
+def main():
+    if solve(*parse()):
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == '__main__':
+    main()
