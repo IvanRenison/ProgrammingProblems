19629082b00781dec0d8ba02f04fb92d5db2f8df
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-04-13 19:52:18 -0300

    Add `Template_bool.cpp`

---
+++ Templates/Template_bool.cpp
@@ -0,0 +1,26 @@
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+#define fore(i, a, b) for (ull i = a; i < b; i++)
+
+
+bool solve(void) {
+
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  if (solve()) {
+    cout << "Yes\n";
+  } else {
+    cout << "No\n";
+  }
+
+  return EXIT_SUCCESS;
+}
