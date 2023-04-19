a5c7f4b48e2fae9ab2ba450e65b99aca7e563c49
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-04-19 10:44:17 -0300

    Fix `binary_search` in `libary.cpp`

--- Templates/library.cpp
+++ Templates/library.cpp
@@ -109,17 +109,24 @@ class segmentsWeights {
   }
 };
 
-template <typename T> T binary_search(bool f(T), vector<T> it) {
+/* Find the minimum index for witch f is true or or it.size() if none if true */
+template <typename T> ull binary_search(function<bool(T)> f, vector<T> it) {
   ull lower = 0;
   ull upper = it.size();
+  if (f(it[lower])) {
+    return lower;
+  }
+  if (!f(it[upper - 1])) {
+    return upper;
+  }
   while (lower + 1 < upper) {
     ull m = (lower + upper) / 2;
-    if (f(ir[m])) {
-      lower = m;
-    } else {
+    if (f(it[m])) {
       upper = m;
+    } else {
+      lower = m;
     }
   }
 
-  return lower;
+  return upper;
 }
