1b43404ab886d08a106a1d3cd9fe6d607f8073d2
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-05-01 10:25:13 -0300

    Problem C of 2021 TAP

--- ICPC contests/2021-2022 ACM-ICPC Brazil Subregional Programming Contest/C.cpp
+++ ICPC contests/2021-2022 ACM-ICPC Brazil Subregional Programming Contest/C.cpp
@@ -1,62 +1,86 @@
 // https://codeforces.com/gym/103388/problem/C
 
-#include <iostream>
-#include <tuple>
-#include <vector>
-
-#define uint unsigned int
+#include <bits/stdc++.h>
 
 using namespace std;
 
-uint pow(uint b, uint e) {
-    uint res = 1;
-    for (uint i = 0; i < e; i++) {
-        res *= b;
-    }
-    return res;
+typedef unsigned long long ull;
+#define fore(i, a, b) for (ull i = a; i < b; i++)
+
+// Calculate de mod of an individual digit, counting from 0
+// (0 is the least significant digit)
+// So it calculates d * B^i % (B+1)
+ull individual_mod(ull B, ull d, ull i) {
+  if (i % 2 == 0) {
+    return d;
+  } else {
+    return B + 1 - d;
+  }
 }
 
-tuple<int, int> solve(uint B, uint L, vector<uint> Ds) {
+ull full_mod(ull B, vector<ull> Ds) {
+  ull L = Ds.size();
+  ull ans = 0;
+  fore(i, 0, L) {
+    ans = (ans + individual_mod(B, Ds[i], i)) % (B + 1);
+  }
 
-    uint rem = 0;
-    {
-        uint sum = 0;
-        for (uint i = 0; i < L; i++) {
-            // sum += Ds[i] * B^(L-1-i);
-            sum += Ds[i] * pow(B, L-1-i);
-        }
-        rem = sum % (B + 1);
+  return ans;
+}
+
+optional<pair<ull, ull>> solve(ull B, vector<ull> Ds) {
+  ull L = Ds.size();
+
+  reverse(Ds.begin(), Ds.end());
+
+  ull m = full_mod(B, Ds);
+
+  if (m == 0) {
+    return {{0, 0}};
+  }
+
+  for (ull i = L; i > 0;) {
+    i--;
+
+    ull d = Ds[i];
+
+    if (i % 2 == 0) {
+      // m2 = d
+      if (d >= m) {
+        ull d1 = d - m;
+        return {{L - i, d1}};
+      }
+    } else {
+      // m1 = B + 1 - d
+      if (d >= B + 1 - m) {
+        ull d1 = d - (B + 1 - m);
+        return {{L - i, d1}};
+      }
     }
+  }
 
-    if (rem == 0) {
-        return {0, 0};
-    }
-
-    for (uint j = 0; j < L; j++) {
-        uint i = L - 1 - j;
-        int new_d = Ds[j] - (i % 2 == 0 ? rem : B + 1 - rem);
-        if (new_d >= 0) {
-            return {j + 1, new_d};
-        }
-    }
-
-    return {-1, -1};
+  return {};
 }
 
 int main(void) {
 
-    uint B, L;
-    cin >> B >> L;
+  ull B, L;
+  cin >> B >> L;
 
-    vector<uint> Ds(L);
-    for (uint i = 0; i < L; i++) {
-        cin >> Ds[i];
-    }
+  vector<ull> Ds(L);
+  for (ull i = 0; i < L; i++) {
+    cin >> Ds[i];
+  }
 
-    int res1, res2;
-    tie(res1, res2) = solve(B, L, Ds);
+  optional<pair<ull, ull>> res = solve(B, Ds);
 
-    cout << res1 << " " << res2;
+  if (res.has_value()) {
+    auto [r1, r2] = res.value();
+    cout << r1 << " " << r2 << "\n";
+  } else {
+    cout << "-1 -1"
+         << "\n";
+  }
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
