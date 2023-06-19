a3667a5b3474a813e8ae4ecb844db8791049f9d3
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-06-19 09:50:46 -0300

    Update sieve in `library.cpp`

--- Templates/library.cpp
+++ Templates/library.cpp
@@ -3,39 +3,39 @@ using namespace std;
 
 typedef unsigned long long ull;
 
-/* For each number until N get the smallest prime that divides it */
-vector<ull> get_smallest_prime_factors(ull N) {
-  vector<ull> smallest_prime_factors(N);
+#define MAXN 100001
 
-  for (ull i = 0; i < N; i++) {
+/* For each number until N get the smallest prime that divides it */
+vector<ull> smallest_prime_factors(MAXN);
+void sieve(void) {
+  for (ull i = 0; i < MAXN; i++) {
     smallest_prime_factors[i] = i;
   }
 
-  for (ull i = 2; i * i < N; i++) {
+  for (ull i = 2; i * i < MAXN; i++) {
     if (smallest_prime_factors[i] == i) {
-      for (ull j = i * i; j < N; j += i) {
+      for (ull j = i * i; j < MAXN; j += i) {
         if (smallest_prime_factors[j] == j) {
           smallest_prime_factors[j] = i;
         }
       }
     }
   }
-
-  return smallest_prime_factors;
 }
 
-/* smallest prime factor has to have the smallest ptime factor of each number until n */
-vector<ull> prime_factors(vector<ull> smallest_prime_factors, ull n) {
-  if (n == 1) {
+/* smallest_prime_factors has to have the smallest prime factor of each number until x */
+vector<pair<ull, ull>> prime_factors(ull x) {
+  if (x == 1) {
     return {};
   } else {
-    vector<ull> factors = {smallest_prime_factors[n]};
-    n = ull(n / smallest_prime_factors[n]);
-    while (n != 1) {
-      if (smallest_prime_factors[n] != factors.back()) {
-        factors.push_back(smallest_prime_factors[n]);
+    vector<pair<ull, ull>> factors = {{smallest_prime_factors[x], 1}};
+    x = ull(x / smallest_prime_factors[x]);
+    while (x != 1) {
+      if (smallest_prime_factors[x] != factors.back().first) {
+        factors.push_back({smallest_prime_factors[x], 0});
       }
-      n = ull(n / smallest_prime_factors[n]);
+      factors.back().second++;
+      x = ull(x / smallest_prime_factors[x]);
     }
     return factors;
   }
