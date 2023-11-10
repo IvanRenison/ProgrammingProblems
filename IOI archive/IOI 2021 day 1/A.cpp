4192ed8fa17ba91c3d15af759de8557c01930d7e
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-11-10 12:09:10 -0300

    Task 2 of problem A of IOI 2021 day 1

--- IOI archive/IOI 2021 day 1/A.cpp
+++ IOI archive/IOI 2021 day 1/A.cpp
@@ -5,123 +5,99 @@
 using namespace std;
 
 typedef unsigned long long ull;
-typedef vector<int> vi;
 #define fore(i, a, b) for (ull i = a; i < b; i++)
 
 #include "candies.h"
 
-/**
- * Author: Simon Lindholm
- * Date: 2015-09-12
- * License: CC0
- * Source: me
- * Description: When you need to dynamically allocate many objects and don't care about freeing them.
- * "new X" otherwise has an overhead of something like 0.05us + 16 bytes per allocation.
- * Status: tested
- */
-// Either globally or in a single class:
-static char buf[450 << 20];
-void* operator new(size_t s) {
-  static size_t i = sizeof buf;
-  assert(s < i);
-  return (void*)&buf[i -= s];
-}
-void operator delete(void*) {}
+struct STree { // example: range sum with range addition
+	typedef ull T; typedef ull L; // T: data type, L: lazy type
+	constexpr static T tneut = 0; constexpr static L lneut = 0; // neutrals
+	T oper(T a, T b){return a+b;}
+	T apply(T v, L l, int s, int e) {return v+l*(e-s);} // new st according to lazy
+	L prop(L a, L b) {return a+b;} // cumulative effect of lazy
 
-/**
- * Author: Simon Lindholm
- * Date: 2016-10-08
- * License: CC0
- * Source: me
- * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
- * Can be changed to other things.
- * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
- * Time: O(\log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
- * Status: stress-tested a bit
- */
-const int inf = 1e9;
-struct Node {
-  Node *l = 0, *r = 0;
-  int lo, hi, mset = inf, madd = 0, val = -inf;
-  Node(int lo, int hi) : lo(lo), hi(hi) {} // Large interval of -inf
-  Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-    if (lo + 1 < hi) {
-      int mid = lo + (hi - lo) / 2;
-      l = new Node(v, lo, mid);
-      r = new Node(v, mid, hi);
-      val = max(l->val, r->val);
-    } else
-      val = v[lo];
-  }
-  int query(int L, int R) {
-    if (R <= lo || hi <= L)
-      return -inf;
-    if (L <= lo && hi <= R)
-      return val;
-    push();
-    return max(l->query(L, R), r->query(L, R));
-  }
-  void set(int L, int R, int x) {
-    if (R <= lo || hi <= L)
-      return;
-    if (L <= lo && hi <= R)
-      mset = val = x, madd = 0;
-    else {
-      push(), l->set(L, R, x), r->set(L, R, x);
-      val = max(l->val, r->val);
-    }
-  }
-  void add(int L, int R, int x) {
-    if (R <= lo || hi <= L)
-      return;
-    if (L <= lo && hi <= R) {
-      if (mset != inf)
-        mset += x;
-      else
-        madd += x;
-      val += x;
-    } else {
-      push(), l->add(L, R, x), r->add(L, R, x);
-      val = max(l->val, r->val);
-    }
-  }
-  void push() {
-    if (!l) {
-      int mid = lo + (hi - lo) / 2;
-      l = new Node(lo, mid);
-      r = new Node(mid, hi);
-    }
-    if (mset != inf)
-      l->set(lo, hi, mset), r->set(lo, hi, mset), mset = inf;
-    else if (madd)
-      l->add(lo, hi, madd), r->add(lo, hi, madd), madd = 0;
-  }
-};
+	vector<T> st;vector<L> lazy;int n;
+	STree(int n): st(4*n+5,tneut), lazy(4*n+5,lneut), n(n) {}
+	void init(int k, int s, int e, const vector<T>& a){
+		lazy[k]=lneut;
+		if(s+1==e){st[k]=a[s];return;}
+		int m=(s+e)/2;
+		init(2*k,s,m,a);init(2*k+1,m,e,a);
+		st[k]=oper(st[2*k],st[2*k+1]);
+	}
+	void push(int k, int s, int e){
+		if(lazy[k] == lneut)return; // if neutral, nothing to do
+		st[k]=apply(st[k], lazy[k], s, e);
+		if(s+1<e){ // propagate to children
+			lazy[2*k]=prop(lazy[2*k],lazy[k]);
+			lazy[2*k+1]=prop(lazy[2*k+1],lazy[k]);
+		}
+		lazy[k]=lneut; // clear node lazy
+	}
+	void upd(int k, int s, int e, int a, int b, L v){
+		push(k,s,e);
+		if(s>=b||e<=a)return;
+		if(s>=a&&e<=b){
+			lazy[k]=prop(lazy[k], v); // accumulate lazy
+			push(k,s,e);return;
+		}
+		int m=(s+e)/2;
+		upd(2*k,s,m,a,b,v);upd(2*k+1,m,e,a,b,v);
+		st[k]=oper(st[2*k],st[2*k+1]);
+	}
+	T query(int k, int s, int e, int a, int b){
+		if(s>=b||e<=a)return tneut;
+		push(k,s,e);
+		if(s>=a&&e<=b)return st[k];
+		int m=(s+e)/2;
+		return oper(query(2*k,s,m,a,b),query(2*k+1,m,e,a,b));
+	}
+	void init(const vector<T>& a){init(1,0,n,a);}
+	void upd(int a, int b, L v){upd(1,0,n,a,b,v);}
+	T query(int a, int b){return query(1,0,n,a,b);}
+}; // usage: STree rmq(n);rmq.init(x);rmq.upd(s,e,v);rmq.query(s,e);
 
 vector<int>
 distribute_candies(vector<int> c, vector<int> l, vector<int> r, vector<int> v) {
   ull n = c.size();
   ull q = l.size();
 
-  vector<vector<ull>> starts(n);
-  vector<vector<ull>> ends(n);
+  if (n <= 2000 && q <= 2000) {
+    vector<vector<ull>> starts(n);
+    vector<vector<ull>> ends(n);
 
-  fore(i, 0, q) {}
+    fore(i, 0, q) {}
 
-  vector<int> ans(n);
+    vector<int> ans(n);
 
-  fore(i, 0, q) {
-    if (v[i] > 0) {
-      fore(j, l[i], r[i] + 1) {
-        ans[j] = min(ans[j] + v[i], c[j]);
-      }
-    } else {
-      fore(j, l[i], r[i] + 1) {
-        ans[j] = max(0, ans[j] + v[i]);
+    fore(i, 0, q) {
+      if (v[i] > 0) {
+        fore(j, l[i], r[i] + 1) {
+          ans[j] = min(ans[j] + v[i], c[j]);
+        }
+      } else {
+        fore(j, l[i], r[i] + 1) {
+          ans[j] = max(0, ans[j] + v[i]);
+        }
       }
     }
-  }
 
-  return ans;
+    return ans;
+  } else {
+
+    STree st(n);
+
+    fore(i, 0, q) {
+      int li = l[i], ri = r[i], vi = v[i];
+      st.upd(li, ri + 1, vi);
+    }
+
+    vector<int> ans(n);
+    fore(i, 0, n) {
+      ans[i] = min((ull)c[i], st.query(i, i + 1));
+    }
+
+    return ans;
+
+  }
 }
