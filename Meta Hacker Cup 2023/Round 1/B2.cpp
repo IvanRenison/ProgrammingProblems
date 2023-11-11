e393cac13628595462b46e7a5874728b12d8dfff
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-11-11 15:52:39 -0300

    Links on meta hacker cup round 1

--- Meta Hacker Cup 2023/Round 1/B2.cpp
+++ Meta Hacker Cup 2023/Round 1/B2.cpp
@@ -1,4 +1,4 @@
-//
+// https://www.facebook.com/codingcompetitions/hacker-cup/2023/round-1/problems/B2
 
 #include <bits/stdc++.h>
 
--- Meta Hacker Cup 2023/Round 1/C1.cpp
+++ Meta Hacker Cup 2023/Round 1/C1.cpp
@@ -1,4 +1,4 @@
-//
+// https://www.facebook.com/codingcompetitions/hacker-cup/2023/round-1/problems/C1
 
 #include <bits/stdc++.h>
 
