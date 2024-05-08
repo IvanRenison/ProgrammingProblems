4bb7fff8c1f051ef0839ff7fc8d3d4533856b929
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-05-07 21:09:48 -0300

    Problem S of AtCoder Educational DP Contest

---
+++ Atcoder/Educational DP Contest/S.cpp
@@ -0,0 +1,197 @@
+// https://atcoder.jp/contests/dp/tasks/dp_s
+
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG 1
+#define _GLIBCXX_DEBUG_PEDANTIC 1
+//#define _GLIBCXX_DEBUG_BACKTRACE 1
+#define _GLIBCXX_CONCEPT_CHECKS 1
+#define _GLIBCXX_SANITIZE_VECTOR 1
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef vector<ull> vu;
+typedef pair<ull, ull> uu;
+typedef vector<uu> vuu;
+typedef tuple<ull, ull, ull> uuu;
+typedef vector<uuu> vuuu;
+typedef tuple<ull, ull, ull, ull> uuuu;
+typedef vector<uuuu> vuuuu;
+typedef tuple<ull, ull, ull, ull, ull> uuuuu;
+typedef vector<uuuuu> vuuuuu;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef tuple<ll, ll, ll> iii;
+typedef vector<iii> viii;
+typedef tuple<ll, ll, ll, ll> iiii;
+typedef vector<iiii> viiii;
+typedef tuple<ll, ll, ll, ll, ll> iiiii;
+typedef vector<iiiii> viiiii;
+typedef long double ld;
+typedef vector<ld> vd;
+typedef pair<ld, ld> dd;
+typedef vector<dd> vdd;
+typedef tuple<ld, ld, ld> ddd;
+typedef vector<ddd> vddd;
+typedef tuple<ld, ld, ld, ld> dddd;
+typedef vector<dddd> vdddd;
+typedef vector<bool> vb;
+
+#define fore(i, a, b) for (ull i = a, gmat = b; i < gmat; i++)
+#define ALL(x) x.begin(), x.end()
+#define SZ(x) (x).size()
+#define DBG(x) cerr << #x << " = " << x << endl
+
+const ull inf = 1ull << 60;
+const ld eps = 1e-7;
+
+// Based on ModularArithmetic of kactl
+
+ll euclid(ll a, ll b, ll& x, ll& y) {
+  if (!b)
+    return x = 1, y = 0, a;
+  ll d = euclid(b, a % b, y, x);
+  return y -= a / b * x, d;
+}
+
+constexpr ull mod = (1e9 + 7);
+struct Mod {
+  ull x;
+  Mod(ull xx) : x(xx % mod) {}
+  Mod() : x(0) {}
+  Mod operator+(Mod b) const {
+    return Mod((x + b.x) >= mod ? x + b.x - mod : x + b.x);
+  }
+  Mod operator-(Mod b) const {
+    return Mod(x >= b.x ? x - b.x : x + mod - b.x);
+  }
+  Mod operator*(Mod b) const {
+    return Mod((x * b.x) % mod);
+  }
+  Mod operator+=(Mod b) {
+    return *this = *this + b;
+  }
+  Mod operator-=(Mod b) {
+    return *this = *this - b;
+  }
+  Mod operator*=(Mod b) {
+    return *this = *this * b;
+  }
+  Mod operator++() {
+    return *this = *this + Mod(1);
+  }
+  Mod operator--() {
+    return *this = *this - Mod(1);
+  }
+  bool operator==(Mod b) const {
+    return x == b.x;
+  }
+  bool operator!=(Mod b) const {
+    return x != b.x;
+  }
+  bool operator<(Mod b) const {
+    return x < b.x;
+  }
+  bool operator>(Mod b) const {
+    return x > b.x;
+  }
+  bool operator<=(Mod b) const {
+    return x <= b.x;
+  }
+  bool operator>=(Mod b) const {
+    return x >= b.x;
+  }
+
+  Mod invert(Mod a) const {
+    ll x, y, g = euclid(a.x, mod, x, y);
+    assert(g == 1);
+    return Mod((x + mod) % mod);
+  }
+  Mod operator^(ull e) const {
+    if (!e)
+      return Mod(1);
+    Mod r = *this ^ (e / 2);
+    r = r * r;
+    return e & 1 ? *this * r : r;
+  }
+  Mod operator/(Mod b) const {
+    return *this * invert(b);
+  }
+  Mod operator/=(Mod b) {
+    return *this = *this / b;
+  }
+
+  friend ostream& operator<<(ostream& os, Mod m) {
+    os << m.x;
+    return os;
+  }
+  friend istream& operator>>(istream& is, Mod& m) {
+    is >> m.x;
+    return is;
+  }
+
+  operator ull() const {
+    return x;
+  }
+  operator ll() const {
+    return x;
+  }
+};
+
+typedef vector<Mod> vm;
+typedef pair<Mod, Mod> mm;
+typedef vector<mm> vmm;
+typedef tuple<Mod, Mod, Mod> mmm;
+typedef vector<mmm> vmmm;
+typedef tuple<Mod, Mod, Mod, Mod> mmmm;
+typedef vector<mmmm> vmmmm;
+
+Mod solve(const vu& K, ull D) {
+  ull n = K.size();
+
+  vector<vector<vm>> dp(2, vector<vm>(D, vm(2)));
+  // dp[i % 2][sum][less] = number of ways to form a number with the last i digits, sum of digits = sum, and if less then less than the K
+
+  dp[n % 2][0][0] = dp[n % 2][0][1] = 1;
+
+  for (ull i = n - 1; i < n; i--) {
+    dp[i % 2] = vector<vm>(D, vm(2));
+    fore(sum, 0, D) {
+      fore(d, 0, 10) {
+        dp[i % 2][sum][0] += dp[(i + 1) % 2][(sum + D - d) % D][0];
+        if (d < K[i]) {
+          dp[i % 2][sum][1] += dp[(i + 1) % 2][(sum + D - d) % D][0];
+        } else if (d == K[i]) {
+          dp[i % 2][sum][1] += dp[(i + 1) % 2][(sum + D - d) % D][1];
+        }
+      }
+    }
+  }
+
+  Mod ans = dp[0][0][1] - Mod(1); // We don't have to count 0
+
+  return ans;
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string K;
+  ull D;
+  cin >> K >> D;
+  vu ks(K.size());
+  fore(i, 0, K.size()) {
+    ks[i] = K[i] - '0';
+  }
+
+  Mod ans = solve(ks, D);
+  cout << ans << '\n';
+
+  return EXIT_SUCCESS;
+}
