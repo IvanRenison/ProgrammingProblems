352b62bbfc07ad01e1592f4cc9b82d524d7a2f89
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-05-21 14:36:25 -0300

    Problem E of Brazil Fase Zero 2022

---
+++ ICPC contests/Brazil Fase Zero 2022/E.cpp
@@ -0,0 +1,76 @@
+// https://judge.beecrowd.com/en/problems/view/3416
+
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG 1
+#define _GLIBCXX_DEBUG_PEDANTIC 1
+//#define _GLIBCXX_DEBUG_BACKTRACE 1
+#define _GLIBCXX_CONCEPT_CHECKS 1
+#define _GLIBCXX_SANITIZE_VECTOR 1
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef vector<ull> vu;
+typedef pair<ull, ull> uu;
+typedef vector<uu> vuu;
+typedef tuple<ull, ull, ull> uuu;
+typedef vector<uuu> vuuu;
+typedef tuple<ull, ull, ull, ull> uuuu;
+typedef vector<uuuu> vuuuu;
+typedef tuple<ull, ull, ull, ull, ull> uuuuu;
+typedef vector<uuuuu> vuuuuu;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef tuple<ll, ll, ll> iii;
+typedef vector<iii> viii;
+typedef tuple<ll, ll, ll, ll> iiii;
+typedef vector<iiii> viiii;
+typedef tuple<ll, ll, ll, ll, ll> iiiii;
+typedef vector<iiiii> viiiii;
+typedef long double ld;
+typedef vector<ld> vd;
+typedef pair<ld, ld> dd;
+typedef vector<dd> vdd;
+typedef tuple<ld, ld, ld> ddd;
+typedef vector<ddd> vddd;
+typedef tuple<ld, ld, ld, ld> dddd;
+typedef vector<dddd> vdddd;
+typedef vector<bool> vb;
+
+#define fore(i, a, b) for (ull i = a, gmat = b; i < gmat; i++)
+#define ALL(x) x.begin(), x.end()
+#define SZ(x) (x).size()
+#define DBG(x) cerr << #x << " = " << x << endl
+
+const ull inf = 1ull << 60;
+const ld eps = 1e-7;
+
+
+ull solve(ull N, ull L, ull D) {
+  L *= 1000;
+  ull T = N * D;
+
+  ull ans = ((T + L - 1) / L) * L;
+
+  ans /= 1000;
+
+  return ans;
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ull N, L, D;
+  cin >> N >> L >> D;
+
+  auto ans = solve(N, L, D);
+  cout << ans << '\n';
+
+  return EXIT_SUCCESS;
+}
