1ece40ae304f3a4709f79bcf3a5a384ea3e98f13
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-06-02 09:35:13 -0300

    Problem C of Asia Guilin Collegiate 2023

---
+++ ICPC contests/Asia Guilin Collegiate 2023/C.cpp
@@ -0,0 +1,244 @@
+// https://codeforces.com/gym/104768/problem/C
+
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG 1
+#define _GLIBCXX_DEBUG_PEDANTIC 1
+//#define _GLIBCXX_DEBUG_BACKTRACE 1
+#define _GLIBCXX_CONCEPT_CHECKS 1
+#define _GLIBCXX_SANITIZE_VECTOR 1
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef vector<ull> vu;
+typedef pair<ull, ull> uu;
+typedef vector<uu> vuu;
+typedef tuple<ull, ull, ull> uuu;
+typedef vector<uuu> vuuu;
+typedef tuple<ull, ull, ull, ull> uuuu;
+typedef vector<uuuu> vuuuu;
+typedef tuple<ull, ull, ull, ull, ull> uuuuu;
+typedef vector<uuuuu> vuuuuu;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef tuple<ll, ll, ll> iii;
+typedef vector<iii> viii;
+typedef tuple<ll, ll, ll, ll> iiii;
+typedef vector<iiii> viiii;
+typedef tuple<ll, ll, ll, ll, ll> iiiii;
+typedef vector<iiiii> viiiii;
+typedef long double ld;
+typedef vector<ld> vd;
+typedef pair<ld, ld> dd;
+typedef vector<dd> vdd;
+typedef tuple<ld, ld, ld> ddd;
+typedef vector<ddd> vddd;
+typedef tuple<ld, ld, ld, ld> dddd;
+typedef vector<dddd> vdddd;
+typedef vector<bool> vb;
+
+#define fore(i, a, b) for (ull i = a, gmat = b; i < gmat; i++)
+#define ALL(x) x.begin(), x.end()
+#define SZ(x) (x).size()
+#define DBG(x) cerr << #x << " = " << x << endl
+
+const ull inf = 1ull << 60;
+const ld eps = 1e-7;
+
+// Based on ModularArithmetic of kactl
+constexpr ull mod = 998244353; // Prime number for division to work
+struct Mod {
+  ull x;
+  Mod(ull xx) : x(xx % mod) {}
+  Mod() : x(0) {}
+  Mod operator+(Mod b) const {
+    return Mod((x + b.x) >= mod ? x + b.x - mod : x + b.x);
+  }
+  Mod operator-(Mod b) const {
+    return Mod(x >= b.x ? x - b.x : x + mod - b.x);
+  }
+  Mod operator*(Mod b) const {
+    return Mod((x * b.x) % mod);
+  }
+  Mod operator+=(Mod b) {
+    return *this = *this + b;
+  }
+  Mod operator-=(Mod b) {
+    return *this = *this - b;
+  }
+  Mod operator*=(Mod b) {
+    return *this = *this * b;
+  }
+  Mod operator++() {
+    return *this = *this + Mod(1);
+  }
+  Mod operator--() {
+    return *this = *this - Mod(1);
+  }
+  bool operator==(Mod b) const {
+    return x == b.x;
+  }
+  bool operator!=(Mod b) const {
+    return x != b.x;
+  }
+  bool operator<(Mod b) const {
+    return x < b.x;
+  }
+  bool operator>(Mod b) const {
+    return x > b.x;
+  }
+  bool operator<=(Mod b) const {
+    return x <= b.x;
+  }
+  bool operator>=(Mod b) const {
+    return x >= b.x;
+  }
+
+  Mod invert(Mod a) const {
+    Mod r = a ^ (mod - 2);
+    return r;
+  }
+  Mod operator^(ull e) const {
+    Mod r = Mod(1);
+    Mod b = *this;
+    while (e > 0) {
+      if (e & 1) {
+        r *= b;
+      }
+      b *= b;
+      e /= 2;
+    }
+    return r;
+  }
+  Mod operator/(Mod b) const {
+    return *this * invert(b);
+  }
+  Mod operator/=(Mod b) {
+    return *this = *this / b;
+  }
+
+  friend ostream& operator<<(ostream& os, Mod m) {
+    os << m.x;
+    return os;
+  }
+  friend istream& operator>>(istream& is, Mod& m) {
+    is >> m.x;
+    return is;
+  }
+
+  operator ull() const {
+    return x;
+  }
+  operator ll() const {
+    return x;
+  }
+};
+
+typedef vector<Mod> vm;
+typedef pair<Mod, Mod> mm;
+typedef vector<mm> vmm;
+typedef tuple<Mod, Mod, Mod> mmm;
+typedef vector<mmm> vmmm;
+typedef tuple<Mod, Mod, Mod, Mod> mmmm;
+typedef vector<mmmm> vmmmm;
+
+const ull LIM = 4e5 + 1;
+vector<vu> divs(LIM);
+
+void init() {
+  fore(i, 1, LIM) {
+    for (ull j = i; j < LIM; j += i) {
+      divs[j].push_back(i);
+    }
+  }
+}
+
+
+struct Z2Base {
+  vu bs;
+  ull red(ull x) const {
+    for (ull b : bs) {
+      x = min(x, x ^ b);
+    }
+    return x;
+  }
+  void add(ull x) {
+    x = red(x);
+    if (x != 0) {
+      bs.push_back(x);
+      sort(ALL(bs), greater<>());
+    }
+  }
+  bool in(ull x) const {
+    return red(x) == 0;
+  }
+  ull dim() const {
+    return bs.size();
+  }
+};
+
+
+Mod solve(const vu& as) {
+  ull n = as.size();
+
+  vu counts(n + 1);
+  for (ull a : as) {
+    counts[a]++;
+  }
+
+  Mod ans = 0;
+  {
+    Z2Base B;
+    for (ull a : as) {
+      B.add(a);
+    }
+
+    ans += (Mod(2) ^ (n - B.dim())) - Mod(1);
+  }
+
+  fore(x, 1, 2 * n + 1) {
+    Z2Base B;
+    ull m = 0;
+
+    for (ull d : divs[x]) {
+      if (d <= n && counts[d] > 0) {
+        B.add(d);
+        m += counts[d];
+      }
+    }
+
+    if (m > 0 && B.in(x)) {
+      Mod this_ans = Mod(2) ^ (m - B.dim());
+      ans += this_ans;
+    }
+  }
+
+  return ans;
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  init();
+
+  ull t;
+  cin >> t;
+  while (t--) {
+    ull n;
+    cin >> n;
+    vu as(n);
+    for (ull& a : as) {
+      cin >> a;
+    }
+    auto ans = solve(as);
+    cout << ans << '\n';
+  }
+
+  return EXIT_SUCCESS;
+}
