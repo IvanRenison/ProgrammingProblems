179a58576a82e5bfe44abeac51b8c64852792e00
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-06-06 16:24:57 -0300

    Problem Point Set Range Composite of Library checker

---
+++ Library checker/Point Set Range Composite.cpp
@@ -0,0 +1,210 @@
+// https://judge.yosupo.jp/problem/point_set_range_composite
+
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG 1
+#define _GLIBCXX_DEBUG_PEDANTIC 1
+//#define _GLIBCXX_DEBUG_BACKTRACE 1
+#define _GLIBCXX_CONCEPT_CHECKS 1
+#define _GLIBCXX_SANITIZE_VECTOR 1
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef vector<ull> vu;
+typedef pair<ull, ull> uu;
+typedef vector<uu> vuu;
+typedef tuple<ull, ull, ull> uuu;
+typedef vector<uuu> vuuu;
+typedef tuple<ull, ull, ull, ull> uuuu;
+typedef vector<uuuu> vuuuu;
+typedef tuple<ull, ull, ull, ull, ull> uuuuu;
+typedef vector<uuuuu> vuuuuu;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef tuple<ll, ll, ll> iii;
+typedef vector<iii> viii;
+typedef tuple<ll, ll, ll, ll> iiii;
+typedef vector<iiii> viiii;
+typedef tuple<ll, ll, ll, ll, ll> iiiii;
+typedef vector<iiiii> viiiii;
+typedef long double ld;
+typedef vector<ld> vd;
+typedef pair<ld, ld> dd;
+typedef vector<dd> vdd;
+typedef tuple<ld, ld, ld> ddd;
+typedef vector<ddd> vddd;
+typedef tuple<ld, ld, ld, ld> dddd;
+typedef vector<dddd> vdddd;
+typedef vector<bool> vb;
+
+#define fore(i, a, b) for (ull i = a, gmat = b; i < gmat; i++)
+#define ALL(x) x.begin(), x.end()
+#define SZ(x) (x).size()
+#define DBG(x) cerr << #x << " = " << x << endl
+
+const ull inf = 1ull << 60;
+const ld eps = 1e-7;
+
+// Based on ModularArithmetic of kactl
+constexpr ull mod = 998244353; // Prime number for division to work
+struct Mod {
+  ull x;
+  Mod(ull xx) : x(xx % mod) {}
+  Mod() : x(0) {}
+  Mod operator+(Mod b) const {
+    return Mod((x + b.x) >= mod ? x + b.x - mod : x + b.x);
+  }
+  Mod operator-(Mod b) const {
+    return Mod(x >= b.x ? x - b.x : x + mod - b.x);
+  }
+  Mod operator*(Mod b) const {
+    return Mod((x * b.x) % mod);
+  }
+  Mod operator+=(Mod b) {
+    return *this = *this + b;
+  }
+  Mod operator-=(Mod b) {
+    return *this = *this - b;
+  }
+  Mod operator*=(Mod b) {
+    return *this = *this * b;
+  }
+  Mod operator++() {
+    return *this = *this + Mod(1);
+  }
+  Mod operator--() {
+    return *this = *this - Mod(1);
+  }
+  bool operator==(Mod b) const {
+    return x == b.x;
+  }
+  bool operator!=(Mod b) const {
+    return x != b.x;
+  }
+  bool operator<(Mod b) const {
+    return x < b.x;
+  }
+  bool operator>(Mod b) const {
+    return x > b.x;
+  }
+  bool operator<=(Mod b) const {
+    return x <= b.x;
+  }
+  bool operator>=(Mod b) const {
+    return x >= b.x;
+  }
+
+  Mod invert(Mod a) const {
+    Mod r = a ^ (mod - 2);
+    return r;
+  }
+  Mod operator^(ull e) const {
+    Mod r = Mod(1);
+    Mod b = *this;
+    while (e > 0) {
+      if (e & 1) {
+        r *= b;
+      }
+      b *= b;
+      e /= 2;
+    }
+    return r;
+  }
+  Mod operator/(Mod b) const {
+    return *this * invert(b);
+  }
+  Mod operator/=(Mod b) {
+    return *this = *this / b;
+  }
+
+  friend ostream& operator<<(ostream& os, Mod m) {
+    os << m.x;
+    return os;
+  }
+  friend istream& operator>>(istream& is, Mod& m) {
+    is >> m.x;
+    return is;
+  }
+
+  operator ull() const {
+    return x;
+  }
+  operator ll() const {
+    return x;
+  }
+};
+
+struct T {
+  Mod a, b;
+  Mod operator()(Mod x) {
+    return a * x + b;
+  }
+};
+const T neut = {1, 0};
+struct Tree {
+  T f(T f, T g) {
+    // g(f(x)) = g.a * (f.a * x + f.b) + g.b = g.a * f.a * x + g.a * f.b + g.b
+    T ans;
+    ans.a = g.a * f.a;
+    ans.b = g.a * f.b + g.b;
+
+    return ans;
+  } // (any associative fn)
+  vector<T> s; ll n;
+  Tree(ll n = 0, T def = neut) : s(2*n, def), n(n) {}
+  void upd(ll pos, T val) {
+    for (s[pos += n] = val; pos /= 2;)
+      s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
+  }
+  T query(ll b, ll e) { // query [b, e)
+    T ra = neut, rb = neut;
+    for (b += n, e += n; b < e; b /= 2, e /= 2) {
+      if (b % 2) ra = f(ra, s[b++]);
+      if (e % 2) rb = f(s[--e], rb);
+    }
+    return f(ra, rb);
+  }
+};
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ull N, Q;
+  cin >> N >> Q;
+  vector<T> fs(N);
+  for (auto& [a, b] : fs) {
+    cin >> a >> b;
+  }
+
+  Tree t(N);
+  fore(i, 0, N) {
+    t.upd(i, fs[i]);
+  }
+
+  while (Q--) {
+    ull ty;
+    cin >> ty;
+
+    if (ty == 0) {
+      ull p, a, b;
+      cin >> p >> a >> b;
+      t.upd(p, {a, b});
+    } else {
+      ull l, r, x;
+      cin >> l >> r >> x;
+
+      T f = t.query(l, r);
+      ull ans = f(x);
+
+      cout << ans << '\n';
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
